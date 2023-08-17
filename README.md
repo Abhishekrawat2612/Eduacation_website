8e05b910ad3dc29a2f20817b6cac3b4fa2ed623d
Abhishek Rawat <abhishekrawat1904@gmail.com>
2023-08-17 12:23:09 +0530

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# Eduacation_website
-I created this free website for those who are interested to grow their coaching classes. 
+# CourseHub - Online Courses and Defense Courses Platform
+
+Welcome to the **CourseHub** repository! This repository contains the code and assets for an innovative online platform that offers a wide range of educational courses, including standard courses and specialized defense courses. The platform is built using HTML, CSS, and JavaScript to provide an intuitive and comprehensive learning experience for students and individuals interested in enhancing their knowledge and skills.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [License](#license)
+- [Contact](#contact)
+
+## Introduction
+
+CourseHub is a dynamic online platform that brings together a diverse range of educational courses, catering to both traditional learning and specialized defense training. This website is designed to empower learners by offering a user-friendly interface, interactive content, and a seamless registration process.
+
+## Features
+
+- **Course Catalog:** Browse through an extensive catalog of courses, ranging from standard academic subjects to specialized defense and security training.
+
+- **Interactive Learning:** Engage with multimedia-rich course content, including video lectures, interactive quizzes, and assignments.
+
+- **Enrollment and Progress Tracking:** Easily enroll in courses of interest and track your progress through intuitive course dashboards.
+
+- **Defense Courses:** Discover a unique selection of defense and security courses, designed to equip individuals with practical skills for critical situations.
+
+- **Secure Registration:** Register and create an account with confidence, knowing that your data and privacy are a top priority.
+
+## Getting Started
+
+Follow these steps to set up a local development environment and explore the CourseHub platform on your machine.
+
+### Prerequisites
+
+- Web browser (Google Chrome, Mozilla Firefox, etc.)
+- Code editor (Visual Studio Code, Sublime Text, etc.)
+
+### Installation
+
+1. Clone this repository to your local machine using the following command:
+   ```bash
+   git clone https://github.com/Abhishekrawat2612/Eduacation_website.git
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd 
+   ```
+
+3. Open the `index.html` file in your preferred web browser to access the CourseHub platform locally.
+
+## Usage
+
+Feel free to explore the CourseHub platform, enroll in courses, and experience the learning journey firsthand. Customize the platform's appearance, add new courses, or modify existing content according to your preferences by editing the HTML, CSS, and JavaScript files.
+
+## Contributions
+
+We welcome contributions from the community to enhance the CourseHub platform. If you encounter any issues or have suggestions for improvements, please [open an issue](https://github.com/Abhishekrawat2612/Eduacation_website.git) or submit a pull request.
+
+## Contact
+
+For inquiries or further information, please contact our team at abhishekrawat1904@gmail.com or visit our website 
+
+---
+
+Thank you for your interest in CourseHub! We believe in the power of education and lifelong learning. Your feedback and contributions play a vital role in creating a vibrant and effective learning community.
---
+++ component/header.js
@@ -0,0 +1,95 @@
+function header(){
+    return `
+    <header class="header rs-nav">
+		<div class="top-bar">
+			<div class="container">
+				<div class="row d-flex justify-content-between">
+					<div class="topbar-left">
+						<ul>
+							<li><strong><i class="fa fa-envelope-o"></i>info@practicalconcept.in</strong></li>
+						</ul>
+					</div>
+					<div class="topbar-right">
+						<ul>
+							<li><i class="fa fa-phone"> <strong> 089-29144429</strong></i></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="sticky-header navbar-expand-lg">
+            <div class="menu-bar clearfix">
+                <div class="container clearfix">
+					<div class="menu-logo">
+					<a href="../index.html"><img src="../assets/images/for white bg-2.png" alt="logoImage"></a>
+				</div>
+                    <button class="navbar-toggler collapsed menuicon justify-content-end" type="button" data-toggle="collapse" data-target="#menuDropdown" aria-controls="menuDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+                    <div class="secondary-menu">
+                        <div class="secondary-inner">
+                            <ul>
+								<li><a href="https://www.facebook.com/practicalconceptcourse/" class="btn-link"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://in.linkedin.com/company/practical-concept" class="btn-link"><i class="fa fa-linkedin"></i></a></li>
+								
+							</ul>
+						</div>
+                    </div>
+                    <div class="nav-search-bar">
+                        <form action="#">
+                            <input name="search" value="" type="text" class="form-control" placeholder="Type to search">
+                            <span><i class="ti-search"></i></span>
+                        </form>
+						<span id="search-remove"><i class="ti-close"></i></span>
+                    </div>
+                    <div class="menu-links navbar-collapse collapse justify-content-start" id="menuDropdown">
+					<div class="menu-logo">
+						<a href="../index.html"><img src="../assets/images/for white bg-2.png" alt=""></a>
+					</div>
+                        <ul class="nav navbar-nav">	
+							<li><a href="../index.html">Home</a></li>
+							
+							<li><a href="../about.html">About Us</a></li>
+								<li><a href="javascript:;">Our Courses<i class="fa fa-chevron-down"></i></a>
+								<ul class="sub-menu">
+									<li><a href="../our-cources-subpages/it-programs.html">IT PROGRAMS </a>
+										
+									</li>
+										
+										<li>
+											<a href="../our-cources-subpages/defence-program.html">DEFENCE PROGRAM </a>
+										</li>
+											
+								</ul>
+							</li>
+						
+							<li><a href="javascript:;">Assistance<i class="fa fa-chevron-down"></i></a>
+								<ul class="sub-menu">
+									<li><a href="../academic-writing.html">Academice/Thesis Writing</a></li>
+									<li><a href="../project-assistance.html">Project Assistance</a></li>
+									<li><a href="../job-assistance.html">Job Assistance</a></li>	
+								</ul>
+							</li>
+
+							<li><a href="../resourceSubPages/suggestedReading.html">Resources<i class="fa fa-chevron-down"></i></a>
+								<ul class="sub-menu">
+									<li><a href="../resourceSubPages/suggestedReading.html">Suggested Reading</a></li>
+								</ul>
+							</li>
+							<li><a href="../contact.html">Contact</a></li>
+						</ul>
+						<div class="nav-social-link">
+							<li><a href="https://www.facebook.com/practicalconceptcourse/" class="btn-link"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://in.linkedin.com/company/practical-concept" class="btn-link"><i class="fa fa-linkedin"></i></a></li>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    `
+}
+
+export default header;
